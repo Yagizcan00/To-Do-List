198edb6778edbd5ceb313219eba83f2e6da5b9f3
Yağızcan Şahin <yagizcansahin68@gmail.com>
2023-01-09 15:44:32 +0300

    Update 09/01/23 --- 15.44

--- myapp/src/Components/Main/List.js
+++ myapp/src/Components/Main/List.js
@@ -6,16 +6,27 @@ function List({ toDos, setToDos }) {
         localStorage.setItem("key", JSON.stringify([...toDos]))
     }, [toDos])
 
+    const Delete = event => {
+        console.log(event);
+        setToDos(oldValues => {
+            return oldValues.filter((_, i) => i !== event)
+        })
+    }
+
+    const Check = () => {
+
+    }
+
     return (
         <div>
             <ul>
                 {toDos.map((item) => (
-                    <div style={{ flexDirection: 'row', display: 'flex'}}>
+                    <div style={{ flexDirection: 'row', display: 'flex' }}>
                         <li key={item.id}>
                             {item.id} - {item.name}
                         </li>
-                        <button>Delete</button>
-                        <button>Check</button>
+                        <button id={item.id} onClick={() => Delete(item.id)}>Delete</button>
+                        <button id={item.id} onClick={Check}>Check</button>
                     </div>
                 ))}
             </ul>
